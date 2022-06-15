1e778d12183a32ec9896cd59c1976f1f644e587a
Alexander Flynn <lex@thorflyn.com>
2022-06-15 16:46:20 -0400

    Some Mods. Add locals and TF required_providers

--- makeEC2.tf
+++ makeEC2.tf
@@ -1,8 +1,31 @@
+##Also, the original didn't use local/locals. Adding that in here just
+##for repetition and learning. Doesn't really save much in this case
+locals {
+    region = "us-west-2"
+    profile = "maslow"
+    tags = { 
+        Owner       = "ForEssent"
+        Environment = "dev"
+        Name = "HelloWorld"
+    }
+}
+
+##This code is not required for aws provider - but including it here
+##to keep the idea in my mind
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.18"
+    }
+  }
+}
+
 ##this is for the most part straight from the Terraform Documentation
 
 provider "aws" {
-  region = "us-west-2"
-  profile = "maslow"
+  region = local.region
+  profile = local.profile
 }
 
 data "aws_ami" "ubuntu" {
@@ -25,7 +48,5 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  tags = local.tags
 }
